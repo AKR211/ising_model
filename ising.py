72bcd65ba47a9e13da5939f125d82f2188290b8b
AKR211 <agneyk.rajeev@niser.ac.in>
2023-06-07 12:49:35 +0530

    save history

--- ising.py
+++ ising.py
@@ -2,6 +2,7 @@ from numpy import *
 from numba import njit
 import matplotlib.pyplot as plt
 from matplotlib import gridspec
+import json
 
 @njit(cache=True)
 def interaction(L,i,j):
@@ -11,22 +12,23 @@ def interaction(L,i,j):
 @njit(cache=True)
 def energy(L):
 	U=0
-	for i in range(L.shape[0]):
-		for j in range(L.shape[0]):
-				U+=interaction(L,i,j)
+	for index,value in ndenumerate(L):
+			U+=interaction(L,index[0],index[1])
 	return -U
 
-size=500
+size=50
 betaJ=0.2
-n=10000000
+n=100000
 
 @njit(cache=True)
 def main():
-	S=[]
-	M=[]
+	Magnetization=[]
+	Energy=[]
 	L=2*random.randint(0,2,(size,size))-ones(size)
+	L_init=L.copy()
+	History=[]
 	U=energy(L)
-	T=L.sum()
+	S=L.sum()
 	for k in range(n):
 		i=random.randint(size)
 		j=random.randint(size)
@@ -34,19 +36,25 @@ def main():
 		if exp(-betaJ*dE)>random.uniform(0,1):
 			L[i,j]=-L[i,j]
 			U+=dE
-			T+=2*L[i,j]
-		S.append(T)
-		M.append(U)
-	return M,L,S
+			S+=2*L[i,j]
+			History.append((i,j))
+		Magnetization.append(S)
+		Energy.append(U)
+	return Energy,L,Magnetization,L_init,History
 
-M,L,S=main()
+Energy,Lattice,Magnetization,L_init,History=main()
 fig=plt.figure(figsize=([10,6]))
 gs = gridspec.GridSpec(2, 2)
 gs.update(wspace = 0.3, hspace = 0.3)
 ax1=plt.subplot(gs[:2,0])
 ax2=plt.subplot(gs[0,1])
 ax3=plt.subplot(gs[1,1])
-ax1.imshow(L)
-ax2.plot(range(1,n+1),M)
-ax3.plot(range(1,n+1),S)
+ax1.imshow(Lattice)
+ax2.plot(range(1,n+1),Energy)
+ax3.plot(range(1,n+1),Magnetization)
 plt.show()
+
+name=str(input('Save?:'))
+if name!='':
+	file=open(name+'.json','w')
+	file.write(json.dumps([L_init.tolist()]+History))
